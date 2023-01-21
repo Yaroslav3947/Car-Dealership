78d629f8ed4aabf9c811a75cc02d0e232e87c53a
Yaroslav <kogutyaroslav3947@gmail.com>
2023-01-21 22:13:08 +0200

    ref

--- source/Worker.cpp
+++ source/Worker.cpp
@@ -17,78 +17,46 @@ void Worker::set_id(const size_t &id) {
 void Worker::printInfo(std::ostream &os) const {
     os << "[Worker: id:" << id << ": "<<  name << ": " << age << "y.o: $"  <<  wage << ": " << number_of_repaired_cars << " cars repaired]";
 }
-
-// void change_repair_status(int id, std::shared_ptr<Client> client) {
-//     std::string fileName = "Clients/" + std::to_string(client->get_id()) + ".txt";
-//     std::ifstream inputFile(fileName);
-//     std::string line;
-//     std::string updatedFile;
-
-//     while (std::getline(inputFile, line)) {
-//         std::string carData[16];
-//         std::stringstream lineStream(line);
-//         int i = 0;
-//         while (lineStream >> carData[i]) {
-//             i++;
-//         }
-
-//         int currentId = std::stoi(carData[0]);
-//         if (currentId == id) {
-//             int currentStatus = std::stoi(carData[12]);
-//             carData[12] = std::to_string(!currentStatus);
-//         }
-//         for (int j = 0; j < 14; j++) {
-//             updatedFile += carData[j] + " ";
-//         }
-//         updatedFile += "\n";
-//     }
-
-//     inputFile.close();
-
-//     std::ofstream outputFile(fileName);
-//     outputFile << updatedFile;
-//     outputFile.close();
-// }
 void change_repair_status(int id, std::shared_ptr<Client> client) {
-    std::string fileName = "Clients/" + std::to_string(client->get_id()) + ".txt";
-    std::ifstream inputFile(fileName);
+    std::string file_name = "Clients/" + std::to_string(client->get_id()) + ".txt";
+    std::ifstream input_file(file_name);
 
-    if (!inputFile.is_open()) {
+    if (!input_file.is_open()) {
         throw FileOpenIssue();
     }
 
-    std::vector<std::string> updatedFile;
+    std::vector<std::string> updated_file;
 
     std::string line;
-    while (std::getline(inputFile, line)) {
+    while (std::getline(input_file, line)) {
         // Use a stringstream to split the line by spaces
-        std::stringstream lineStream(line);
-        std::vector<std::string> carData;
+        std::stringstream line_stream(line);
+        std::vector<std::string> car_data;
         std::string data;
-        while (lineStream >> data) {
-            carData.push_back(data);
+        while (line_stream >> data) {
+            car_data.push_back(data);
         }
         // Check if the current line's ID matches the ID passed to the function
-        if (std::stoi(carData[0]) == id) {
+        if (std::stoi(car_data[0]) == id) {
             // Update the repair status
-            int currentStatus = std::stoi(carData[12]);
-            carData[12] = std::to_string(!currentStatus);
+            int current_status = std::stoi(car_data[12]);
+            car_data[12] = std::to_string(!current_status);
         }
         // Join the elements of the vector into a single string
-        std::string updatedLine = boost::algorithm::join(carData, " ");
+        std::string updatedLine = boost::algorithm::join(car_data, " ");
         // Add the updated line to the vector
-        updatedFile.push_back(updatedLine);
+        updated_file.push_back(updatedLine);
     }
-    inputFile.close();
-    std::ofstream outputFile(fileName);
+    input_file.close();
+    std::ofstream output_file(file_name);
 
-    if (!outputFile.is_open()) {
+    if (!output_file.is_open()) {
         throw FileOpenIssue();
     }
-    for (const auto &line : updatedFile) {
-        outputFile << line << std::endl;
+    for (const auto &line : updated_file) {
+        output_file << line << std::endl;
     }
-    outputFile.close();
+    output_file.close();
 }
 void Worker::repair_car(I_Transport::PtrT car, std::shared_ptr<Client> client) {
     int id_car_to_repair = car->get_id();
